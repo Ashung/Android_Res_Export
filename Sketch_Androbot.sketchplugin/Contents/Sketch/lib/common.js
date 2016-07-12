75e72b73dc6b5d2111e0fae0b1a0bfc77e4548a0
Ashung (iMac) <ashung.hung@gmail.com>
2016-07-12 20:24:09 +0800

    export assets

--- Sketch_Androbot.sketchplugin/Contents/Sketch/lib/common.js
+++ Sketch_Androbot.sketchplugin/Contents/Sketch/lib/common.js
@@ -1,27 +1,141 @@
+//
+// Author: Ashung Hung
+// Email: Ashung.hung@gmail.com
 
+// sketchtool
+const sketchtool = NSBundle.mainBundle().pathForResource_ofType_inDirectory("sketchtool", nil,"sketchtool/bin");
+const sketchmigrate = NSBundle.mainBundle().pathForResource_ofType_inDirectory("sketchmigrate", nil,"sketchtool/bin");
 
-function save_path(context) {
+// imageMagick
+const composite = "/usr/local/bin/composite";
+const convert = "/usr/local/bin/composite";
+
+function convertToAndroidResName(name) {
+    // .replace(/[\u0000-\u0031\/\\\|\?\"\*\:\<\>\.]/g, "_")
+    // .replace(/[^A-Za-z0-9\$\_]/g, "_")
+    return name.replace(/[\u0020-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007F]/g, "_")
+        .replace(/^\d+/, "_")
+        .toLowerCase()
+        .substring(0, 255);
+}
+
+function toast(context, message) {
+    var doc = context.document;
+    doc.showMessage(message);
+}
+
+function getPluginPath(context) {
+    var path = decodeURI(context.plugin.url()).replace("file://", ""));
+    return path;
+}
+
+function getFilePath(context) {
+    var doc = context.document;
+    var path = decodeURI(doc.fileURL()).replace("file://", ""));
+    return path;
+}
+
+function selectFolderDialog(context, message) {
     var doc = context.document;
     if(doc.fileURL()) {
         var defaultDir = [[doc fileURL] URLByDeletingLastPathComponent];
         var panel = [NSOpenPanel openPanel];
-            [panel setMessage:"Where do you want to export the assets?"];
+            [panel setMessage: message];
             [panel setCanChooseDirectories: true];
             [panel setCanChooseFiles: false];
             [panel setCanCreateDirectories: true];
-            [panel setDirectoryURL:defaultDir];
+            [panel setDirectoryURL: defaultDir];
         if ([panel runModal] == NSOKButton) {
             return [panel filename];
         }
     } else {
-        log("NO")
+        toast(context, "Save document first.");
     }
+}
 
-// log( [doc fileURL])
+function sketchtoolExport(sketchFile, outputFolder, itemIds) {
+    var command = sketchtool + ' export slices "' + sketchFile +
+        '" --use-id-for-name="yes" --scales="1, 1.5, 2, 3, 4" --group-contents-only="yes" --overwritin="yes"';
+    if(itemIds) {
+        command += ' --items="' + itemIds + '"';
+    }
+        command += ' --output="' + outputFolder + '"';
+
+    runCommand(command);
+}
+
+function mv(formPath, toPath) {
+    var command = 'mv ' + formPath + ' ' + toPath;
+    runCommand(command);
+}
+
+function mkResDir(rootFolder) {
+    var command = 'cd ' + rootFolder + ' && mkdir -p ' +
+        'res/drawable-mdpi ' +
+        'res/drawable-hdpi ' +
+        'res/drawable-xhdpi ' +
+        'res/drawable-xxhdpi ' +
+        'res/drawable-xxxhdpi';
+    runCommand(command);
+}
+
+function mkMipmapDir(rootFolder) {
+    var command = 'cd ' + rootFolder + ' && mkdir -p ' +
+        'res/mipmap-mdpi ' +
+        'res/mipmap-hdpi ' +
+        'res/mipmap-xhdpi ' +
+        'res/mipmap-xxhdpi ' +
+        'res/mipmap-xxxhdpi';
+    runCommand(command);
+}
+
+function runCommand(command) {
+    var task = NSTask.alloc().init();
+        task.setLaunchPath_(@"/bin/bash");
+        task.setArguments_(NSArray.arrayWithObjects_("-c", command, nil));
+        task.launch();
+        task.waitUntilExit();
+
+    if (task.terminationStatus() != 0) {
+        toast(task.terminationReason());
+    }
+}
+
+////////////
+
+function runCommandWithReturn(command) {
+    var task = NSTask.alloc().init();
+    var pipe = NSPipe.pipe();
+    var errPipe = NSPipe.pipe();
+
+    task.setLaunchPath_(@"/bin/bash");
+    task.setArguments_(NSArray.arrayWithObjects_("-c", command, nil));
+    task.setStandardOutput_(pipe);
+    task.setStandardError_(errPipe);
+    task.launch();
+    task.waitUntilExit();
+
+    var data = errPipe.fileHandleForReading().readDataToEndOfFile();
+    if (data != nil && data.length()) {
+      var message = NSString.alloc().initWithData_encoding_(data, NSUTF8StringEncoding);
+
+      log("11111" + NSException.raise_format_("failed", message))
+      return NSException.raise_format_("failed", message);
+    }
+    data = pipe.fileHandleForReading().readDataToEndOfFile();
+
+    log(NSString.alloc().initWithData_encoding_(data, NSUTF8StringEncoding))
+
+    return NSString.alloc().initWithData_encoding_(data, NSUTF8StringEncoding);
+}
 
 
 
-
+// var createTask = [[NSTask alloc] init];
+// [createTask setLaunchPath:@"/bin/bash"];
+// [createTask setArguments:["-c", command]]
+// [createTask launch]
+// [createTask waitUntilExit]
 
 
 //     // var curPath = [doc fileURL] ? [[[doc fileURL] path] stringByDeletingLastPathComponent] : @"~";
@@ -45,23 +159,3 @@ function save_path(context) {
 //
 //     // return [[savePanel URL] path]
 // return [savePanel filename]
-
-
-
-
-}
-
-function convert_iconset_to_icns(iconsetPath, iconPath) {
-	var createCmd  = "iconutil -c icns \"" + iconsetPath + "\" -o \"" + iconPath + "\""
-	var createTask = [[NSTask alloc] init]
-	[createTask setLaunchPath:@"/bin/bash"]
-	[createTask setArguments:["-c", createCmd]]
-	[createTask launch]
-	[createTask waitUntilExit]
-
-	if ([createTask terminationStatus] == 0) {
-		[doc showMessage:@"Done"]
-	} else {
-		[doc showMessage:@"Failed:" + [createTask terminationReason]]
-	}
-}
