76eaadad9e9cdcd45c8239adf61b8a21089d823e
Ashung Hung <Ashung.hung@gmail.com>
2017-09-04 00:25:44 +0800

    (WIP)

--- Android_Res_Export.sketchplugin/Contents/Sketch/lib/common.js
+++ Android_Res_Export.sketchplugin/Contents/Sketch/lib/common.js
@@ -1,10 +1,22 @@
-//
-// Android Res Export
-// Homepage: https://github.com/Ashung/Android_Res_Export
-// Author: Ashung Hung
-// Email: Ashung.hung@gmail.com
-// License: https://creativecommons.org/licenses/by-sa/4.0
+/*----------------------------------------------------------
 
+Android Res Export
+https://github.com/Ashung/Android_Res_Export
+
+Copyright 2017 Ashung Hung (Ashung.hung@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+----------------------------------------------------------*/
 
 /* =========================================================
     Android
