983574747c4f828e1db999d84156b521906d5171
Ashung <ashung.hung@gmail.com>
2019-03-13 20:03:39 +0800

    Add float precision support. need to update svg2vectordrawable 2.6.10

--- Android_Res_Export.sketchplugin/Contents/Sketch/lib/common.js
+++ Android_Res_Export.sketchplugin/Contents/Sketch/lib/common.js
@@ -182,32 +182,34 @@ function groupFromLayers(layers) {
     } else {
         group = MSLayerGroup.groupFromLayers(layerArray);
     }
-    // group.absoluteInfluenceRect().setRect(group.absoluteInfluenceRect());
     return group;
 }
 
-function addSliceInToGroup(layerGroup, name, format, useInfluenceRect) {
+function addSliceInToGroup(layerGroup, name, format) {
 
     removeSliceInGroup(layerGroup);
 
     var slice = MSSliceLayer.sliceLayerFromLayer(layerGroup);
-    if (useInfluenceRect) {
-        slice.absoluteRect().setRect(layerGroup.absoluteInfluenceRect());
-    }
     slice.setName(name);
-    // slice.moveToLayer_beforeLayer(layerGroup, layerGroup.firstLayer());
     slice.exportOptions().setLayerOptions(2);
     slice.exportOptions().removeAllExportFormats();
-    
     var exportOption = slice.exportOptions().addExportFormat();
     exportOption.setFileFormat(format);
     exportOption.setName("");
     exportOption.setScale(1);
 
-    if (MSApplicationMetadata.metadata().appVersion >= 53) {
-        layerGroup.fixGeometryWithOptions(1);
+    if (
+        !Number.isInteger(layerGroup.absoluteRect().x()) ||
+        !Number.isInteger(layerGroup.absoluteRect().y()) ||
+        !Number.isInteger(layerGroup.absoluteRect().width()) ||
+        !Number.isInteger(layerGroup.absoluteRect().height())
+    ) {
+        var newGroup = groupFromLayers([layerGroup, slice]);
+        newGroup.setName(layerGroup.name());
+        slice.moveToLayer_beforeLayer(newGroup, newGroup.firstLayer());
+        layerGroup.ungroup();
     } else {
-        layerGroup.resizeToFitChildrenWithOption(1);
+        slice.moveToLayer_beforeLayer(layerGroup, layerGroup.firstLayer());
     }
 
     return slice;
@@ -434,6 +436,12 @@ function removeSystemPreference(key) {
     userDefaults.synchronize();
 }
 
+function pasteboardCopy(text) {
+    var pasteboard = NSPasteboard.generalPasteboard();
+    pasteboard.clearContents();
+    pasteboard.setString_forType(text, NSStringPboardType);
+}
+
 /* =========================================================
     Command line tools
 ========================================================= */
@@ -642,9 +650,7 @@ function showCodeWindow(context, code, saveAction) {
             }
 
             if (locationHash == "#copy") {
-                var pasteboard = NSPasteboard.generalPasteboard();
-                pasteboard.clearContents();
-                pasteboard.setString_forType(code, NSStringPboardType);
+                pasteboardCopy(code);
             }
         }
     );
