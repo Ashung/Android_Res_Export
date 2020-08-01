08f6b2011d486cf9ec96dbc9666526a3017bdfd4
Ashung Hung <ashung.hung@gmail.com>
2020-08-01 19:43:06 +0800

    accepts first mouse

--- src/view-vector-drawable-code.js
+++ src/view-vector-drawable-code.js
@@ -18,6 +18,7 @@ export default function () {
         title: 'View Vector Drawable Code',
         resizable: false,
         remembersWindowFrame: true,
+        acceptsFirstMouse: true,
         alwaysOnTop: true
     };
 
